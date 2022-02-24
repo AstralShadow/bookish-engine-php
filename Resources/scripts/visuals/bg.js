92e3091453807dcf445e0e06f5ceac9e872368bb
AzCraft <47869198+AstralShadow@users.noreply.github.com>
2022-02-24 20:31:28 +0200

    [web] Adding background visuals

---
+++ Resources/scripts/visuals/bg.js
@@ -0,0 +1,30 @@
+const enabled = true
+
+var x = 0, y = 0
+
+document.addEventListener("mousemove", function(ev)
+{
+    x = ev.clientX
+    y = ev.clientY
+    var w = window.innerWidth
+    var h = window.innerHeight
+
+    x = (x / w) * -10
+    y = (y / h) * -10
+
+})
+
+var r_x = 0, r_y = 0
+
+requestAnimationFrame(animation)
+function animation()
+{
+    requestAnimationFrame(animation)
+    r_x = (x + r_x * 15) / 16
+    r_y = (y + r_y * 15) / 16
+
+    var pos = r_x + "px " + r_y + "px"
+    document.body.style.backgroundPosition = pos
+
+}
+
