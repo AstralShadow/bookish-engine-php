ffa3d090950391192d8a14ddc0c5ab6be6f5a812
AzCraft <47869198+AstralShadow@users.noreply.github.com>
2023-03-13 23:50:27 +0200

    Core: updating to latest version

--- Extend/redirect.php
+++ Extend/redirect.php
@@ -2,13 +2,13 @@
 
 namespace Extend;
 
-use \Core\Responses\InstantResponse;
+use \Core\Responses\BufferedResponse;
 
 function redirect(?string $uri = null,
                   int $code = 303)
 {
     $next = $uri ?? $_GET["next"] ?? "./";
-    $response = new InstantResponse($code);
+    $response = new BufferedResponse($code);
     $response->setHeader("Location", $next);
     return $response;
 }
